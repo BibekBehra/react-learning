a3466d0a20ef82382d983a76e4a91db3729648f7
Bibek-kumar <57433258+Bibek-kumar@users.noreply.github.com>
2020-03-19 18:01:55 +0530

    Object destructing for array of objects

---
+++ JS/Destructing_object.js
@@ -0,0 +1,9 @@
+const persons = [
+    { name: 'Maximilian', age: 28 },
+    { name: 'Manu', age: 29 },
+    { name: 'Stephanie', age: 27 }
+  ]
+
+const[...personState]=persons
+
+console.log ('abc');
