758b3ae181b9e843e8cedc8be30be3101983b848
Bibek-kumar <57433258+Bibek-kumar@users.noreply.github.com>
2020-03-20 10:05:10 +0530

    Iterate over person component instead of writing it multiple times and delete one by one on click on it

--- src/App.js
+++ src/App.js
@@ -30,36 +30,44 @@ class App extends Component {
     });
 
   }
-
-  
-  
-tooglePersonHandler = () => {
-  const doesShow = this.state.showPerson;
-  this.setState({ showPerson: !doesShow });
-}
-render() {
-  let person =null;
-  if (this.state.showPerson) {
-    person = (
-      <div>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Okay</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'from paragraph')}
-          changed={this.nameChangedHandler} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
-
-    )
+  deletepersonHandler = (personIndex)=>{
+      const persons=this.state.persons;
+      persons.splice(personIndex,1);
+      this.setState({persons:persons});
   }
-  return (
-    <div className="App">
-      <h1> Hi i am a react app </h1>
-      <button onClick={() => this.switchNameHandler('from button')}> switch name </button>
-      <button onClick={this.tooglePersonHandler}> toggle </button>
-      {person}
-    </div>
-  );
-}
+  tooglePersonHandler = () => {
+    const doesShow = this.state.showPerson;
+    this.setState({ showPerson: !doesShow });
+  }
+  render() {
+    let persons = null;
+    if (this.state.showPerson) {
+      persons = (
+        <div>
+          {this.state.persons.map((person,index) => {
+            return <Person
+              click={()=>this.deletepersonHandler(index)}
+              name={person.name}
+              age={person.age} />
+          })}
+        </div>
+      )
+    }
+    // <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Okay</Person>
+    // <Person name={this.state.persons[1].name} 
+    //         age={this.state.persons[1].age}
+    //         click={this.switchNameHandler.bind(this, 'from paragraph')}
+    //         changed={this.nameChangedHandler} />
+    // <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+    return (
+      <div className="App">
+        <h1> Hi i am a react app </h1>
+        <button onClick={() => this.switchNameHandler('from button')}> switch name </button>
+        <button onClick={this.tooglePersonHandler}> toggle </button>
+        {persons}
+      </div>
+    );
+  }
 }
 
 export default App;
