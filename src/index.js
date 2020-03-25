66b9cea0f8cd32f304c132345d181ca79d002769
Bibek-kumar <57433258+Bibek-kumar@users.noreply.github.com>
2020-03-25 19:58:56 +0530

    project folder structure along with leaning out container

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import App from './App';
+import App from './containers/App';
+ 
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
