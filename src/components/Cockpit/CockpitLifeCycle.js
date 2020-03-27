9b2af4ca4ccf8175ba4e834b37a7d176776b963b
Bibek-kumar <57433258+Bibek-kumar@users.noreply.github.com>
2020-03-27 14:48:05 +0530

    useEffect in FC introduced

--- src/components/Cockpit/CockpitLifeCycle.js
+++ src/components/Cockpit/CockpitLifeCycle.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 import styled from "styled-components";
 import classes from "./Cockpit.module.css";
@@ -16,40 +16,23 @@ const StyledButton = styled.button`
 `;
 
 const Cockpit = props => {
-  // useEffect(() => {
-  //   console.log('[Cockpit.js] useEffect');
-  //   // Http request...
-  //   setTimeout(() => {
-  //     alert('Saved data to cloud!');
-  //   }, 1000);
-  //   return () => {
-  //     console.log('[Cockpit.js] cleanup work in useEffect');
-  //   };
-  // }, []);
-
   useEffect(() => {
-    console.log('[Cockpit.js] 2nd useEffect');
-    return () => {
-      console.log('[Cockpit.js] cleanup work in 2nd useEffect');
-    };
+    console.log("[Cockpit.js]   useEffect");
   });
-
-  // useEffect();
 
   let btnClass = classes.simple;
   if (props.showPerson) {
     btnClass = classes.Violet;
   }
-
   return (
     <div className={classes.Cockpit}>
-    {/* Adding cockpit styling inside a div is necassary to apply only cockpit css scope in below items else App scope will be overridden on it */}
+      {/* Adding cockpit styling inside a div is necassary to apply only cockpit css scope in below items else App scope will be overridden on it */}
       <h1>{props.title}</h1>
       <p>This is really working</p>
       <button className={btnClass} onClick={() => props.clicked("from button")}>
-        switch name 
+        switch name
       </button>
-      <StyledButton onClick={props.toogled}> toggle </StyledButton>
+      <StyledButton onClick={props.toogled}> togglePerson </StyledButton>
     </div>
   );
 };
