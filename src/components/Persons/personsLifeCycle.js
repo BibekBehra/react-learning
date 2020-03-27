5d654aee1e54898cd39d1b3ca783f17a49f4463d
Bibek-kumar <57433258+Bibek-kumar@users.noreply.github.com>
2020-03-27 19:55:37 +0530

    [Major]Using Purecomponent instead of shouldComponentUpdate

--- src/components/Persons/personsLifeCycle.js
+++ src/components/Persons/personsLifeCycle.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import Person from "./Person/PersonLifeCycle.js";
 
-class PersonsLifeCycle extends Component {
+class PersonsLifeCycle extends PureComponent {
 //      static getDerivedStateFromProps(props, state) {
 //     console.log('[Persons_LC.js] getDerivedStateFromProps');
 //     return state;
@@ -11,15 +11,15 @@ class PersonsLifeCycle extends Component {
   //   console.log('[Persons_LC.js] componentWillReceiveProps', props);
   // }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    // personLifeCycle component going to be updated ONLY when there is an update on only on the name roperty of 0th index of person array.
-    if(nextProps.persons[0].name !== this.props.persons[0].name){
-      console.log('[Persons_LC.js] shouldComponentUpdate and its going to updated');
-      return true;
-    }
-    console.log('[PersonsLifeCycle.js]  shouldComponentUpdate and its not going updated');
-    return false ;
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   // personLifeCycle component going to be updated ONLY when there is an update on only on the name roperty of 0th index of person array.
+  //   if(nextProps.persons[0].name !== this.props.persons[0].name){
+  //     console.log('[Persons_LC.js] shouldComponentUpdate and its going to updated');
+  //     return true;
+  //   }
+  //   console.log('[PersonsLifeCycle.js]  shouldComponentUpdate and its not going updated');
+  //   return false ;
+  // }
 
 //   getSnapshotBeforeUpdate(prevProps, prevState) {
 //     console.log('[Persons_LC.js] getSnapshotBeforeUpdate');
