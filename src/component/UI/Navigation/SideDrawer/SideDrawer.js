643437a22024930c8c60f629b7d6822d5e3577e9
Bibek-kumar <57433258+Bibek-kumar@users.noreply.github.com>
2020-04-05 02:37:46 +0530

    Added : 1-Sidedrawer 2-Drawertoogle

--- src/component/UI/Navigation/SideDrawer/SideDrawer.js
+++ src/component/UI/Navigation/SideDrawer/SideDrawer.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import Logo from '../../../Logo/Logo.js';
-import NavigationItems from '../NavigationItems/NavigationItems';
-import classes from './SideDrawer.module.css';
-
+import React from "react";
+import Logo from "../../../Logo/Logo.js";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import classes from "./SideDrawer.module.css";
+import Aux from "../../../../hoc/Auxilary.js";
+import BackDrop from "../../../UI/Backdrop/Backdrop.js";
 const SideDrawer = () => {
-    return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-            <Logo/>
-            </div>
-            <nav>
-               <NavigationItems/>
-            </nav>
+  return (
+    <Aux>
+      <BackDrop show></BackDrop>
+      <div className={classes.SideDrawer}>
+        <div className={classes.Logo}>
+          <Logo />
         </div>
-    )
-
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
+  );
 };
 export default SideDrawer;
