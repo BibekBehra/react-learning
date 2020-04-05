5802835ec2d0dc86bcccdfb1245b8a4ab13e2689
Bibek-kumar <57433258+Bibek-kumar@users.noreply.github.com>
2020-04-05 15:53:37 +0530

    Updating data after getting from server

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,36 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
-import axios from 'axios';
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
+import axios from "axios";
 
 class Blog extends Component {
-componentDidMount(){
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-    .then(response=>{
-console.log(response.data);
+  state = {
+    posts: [],
+  };
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map(post => {
+        return {
+          ...post,
+          Author: "Bibek"
+        };
+      });
+      this.setState({ posts: updatedPosts });
     });
-}
+  }
 
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  render() {
+    const posts = this.state.posts.map((post) => {
+      return (
+        <Post key={post.id} title={post.title} author={post.Author}></Post>
+      );
+    });
+    console.log(posts);
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
