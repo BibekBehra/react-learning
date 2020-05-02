ca25045f3267dd09d6f1e27e6cdbb77284713af4
Bibek-kumar <57433258+Bibek-kumar@users.noreply.github.com>
2020-05-02 15:42:04 +0530

    Link vs anchor tag

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-import "./Blog.css";
+// import axios from 'axios';
 import { Route, Link } from "react-router-dom";
-import Posts from "../Blog/Posts/Posts.js";
-import NewPost from "../Blog/NewPost/NewPost";
+
+import "./Blog.css";
+import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
 
 class Blog extends Component {
   render() {
@@ -12,17 +14,26 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <a href="/">Home</a>
+                <Link to="/">Home</Link>
               </li>
               <li>
-                <a href="/new-post">New Post</a>
+                <Link
+                  to={{
+                    pathname: "/new-post",
+                    hash: "#submit",
+                    search: "?quick-submit=true",
+                  }}
+                >
+                  New Post
+                </Link>
               </li>
             </ul>
           </nav>
         </header>
-        {/* <Route Path="/" exact render={()=><h1>Home</h1>}></Route> */}
-        <Route path="/" exact component={Posts}></Route>
-        <Route path="/new-post" component={NewPost}></Route>
+        {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
